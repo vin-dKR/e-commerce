c9c70c3b5c8a57f57290a2ecfafec34f5b6a3107
vin-dKR <vinodkumarmurmu62@gmail.com>
2024-07-20 12:02:02 +0530

    basic footer logolinks

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -7,11 +7,13 @@ import classes from './index.module.scss'
 import { inclusions, noHeaderFooterUrls } from '../../../constants'
 import { Gutter } from '../../Gutter'
 import Image from 'next/image'
-import { Footer } from '../../../../payload/payload-types'
+import { Footer, Media } from '../../../../payload/payload-types'
 import Link from 'next/link'
+import { Button } from '../../Button'
 
 const FooterComponent = ({ footer }: { footer: Footer }) => {
     const usepathname = usePathname()
+    const navItems = footer?.navItems || []
 
     return (
         <footer className={noHeaderFooterUrls.includes(usepathname) ?
@@ -27,8 +29,44 @@ const FooterComponent = ({ footer }: { footer: Footer }) => {
                     ))}
                 </ul>
             </Gutter>
-
             
+            <div className={classes.footer}>
+                <Gutter>
+                    <div className={classes.wrap}>
+                        <Link href="/">
+                            <Image src="/logo-white.svg" alt="logo" width={170} height={50} />
+                        </Link>
+
+                        <p>{footer?.copyright}</p>
+
+                        <div className={classes.socialLinks}>
+                            {navItems.map(item => {
+                                // const icon = ''
+                                const icon = item?.link?.icon as Media
+
+                                return (
+                                    <Button
+                                        key={item.link.label}
+                                        el="link"
+                                        href={item.link.url}
+                                        newTab={true}
+                                        className={classes.socialLinkItem}
+                                    >
+                                        <Image
+                                            src={icon?.url}
+                                            alt='jii'
+                                            width={24}
+                                            height={24}
+                                            className={classes.socialIcon}
+                                        />
+                                    </Button>
+                                )
+                            })}
+                        </div>
+                    </div>
+                </Gutter>
+            </div>
+
         </footer>
     )
 }
--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -22,7 +22,7 @@ const HeaderComponent = ({ header }: { header: Header }) => {
       .join(' ')}>
       <Gutter className={classes.wrap} >
         <Link href='/'>
-          <Image src="/google-logo-transparent.png" alt="logo" width={170} height={50} />
+          <Image src="/logo-white.svg" alt="logo" width={170} height={50} />
         </Link>
 
         <HeaderNav header={header} />
